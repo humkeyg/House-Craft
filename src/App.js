73e571ef828b8af3d2bb506e4592884730cb1fc3
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-05-17 20:25:37 -0700

    file structure change, app.js setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About/>;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact/>;
+    }  
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+      {renderPage()}
+      <Footer/>
     </div>
   );
 }
